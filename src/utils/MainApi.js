2e040632e88e88f3d76f91583b19140bdd3b3e2a
Pavel Kuzmichev <kuzpavel1985@yandex.ru>
2021-06-29 15:11:32 +0500

    3 этап - frontend: на ревью порты.

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3001';
+export const BASE_URL = 'https://api.kuzpavel1985.nomoredomains.icu';
 
 const checkResponse = (res) => (res.ok ? res.json() : Promise.reject(`Error: ${res.status}`));
 
